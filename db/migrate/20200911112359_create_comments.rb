177f59fd2c6fc91462227d123e34f6deceda28bd
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-09-11 16:44:38 +0500

    post, images -> comments polymorphic

--- db/migrate/20200911112359_create_comments.rb
+++ db/migrate/20200911112359_create_comments.rb
@@ -2,6 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.text :content
+      t.references :commentable, polymorphic: true
 
       t.timestamps
     end
